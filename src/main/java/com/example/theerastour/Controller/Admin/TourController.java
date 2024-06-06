d2a1af00f54a1e8bead732b01431808fd5a7269d
lequanghien4u@gmail.com <lequanghien4u@gmail.com>
2024-06-06 23:08:45 +0700

    first commit

---
+++ src/main/java/com/example/theerastour/Controller/Admin/TourController.java
@@ -0,0 +1,5 @@
+package com.example.theerastour.Controller.Admin;
+
+public class TourController {
+
+}
---
+++ src/main/java/com/example/theerastour/TheErasTourApplication.java
@@ -0,0 +1,13 @@
+package com.example.theerastour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheErasTourApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheErasTourApplication.class, args);
+    }
+
+}
